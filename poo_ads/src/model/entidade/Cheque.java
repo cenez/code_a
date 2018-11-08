d51d32354bd7360cb98b5dbe6bf4f79f7d0e9e54
Cenez Araujo <cenezaraujo@gmail.com>
2018-11-08 00:40:51 -0300

    Update

--- poo_ads/src/model/entidade/Cheque.java
+++ poo_ads/src/model/entidade/Cheque.java
@@ -1,20 +1,25 @@
 package model.entidade;
 
-public class Cheque extends Pagamento {
+public class Cheque implements Pagamento {
 	private String numero;
 	private String agencia;
 	private String conta;
 	private String banco;
+	private double value;
+	private Pedido pedido;
 	
-	public Cheque() { super(0); }
-	public Cheque(double _valor) { super(_valor); }	
-	public Cheque(String numero, String agencia, String conta, String banco, double _valor) {
-		super(_valor);
+	public Cheque() { }
+	public Cheque(String numero, String agencia, String conta, String banco, double _valor, Pedido pedido) {
 		this.numero = numero;
 		this.agencia = agencia;
 		this.conta = conta;
 		this.banco = banco;
+		this.pedido = pedido;
 	}
+	public double getValue()               { return this.value; }
+	public void setValue(double valor)     { this.value = valor; }
+	public Pedido getPedido()              { return this.pedido; }
+	public void setPedido(Pedido pedido)   { this.pedido = pedido; }	
 	public String getBanco()               { return banco; }
 	public void setBanco(String banco)     { this.banco = banco; }
 	public String getConta()               { return conta; }
--- poo_ads/src/model/entidade/Pagamento.java
+++ poo_ads/src/model/entidade/Pagamento.java
@@ -1,13 +1,8 @@
 package model.entidade;
 
-public class Pagamento {
-	private double valor;
-	private Pedido pedido;
-	
-	public Pagamento(double _valor) {  this.valor = _valor; }
-
-	public double getValor() { return valor; }
-	public void setValor(double valor) { this.valor = valor; }
-	public Pedido getPedido() { return pedido; }
-	public void setPedido(Pedido pedido) { this.pedido = pedido; }
+public interface Pagamento {
+	public double getValue();
+	public void setValue(double valor);
+	public Pedido getPedido();
+	public void setPedido(Pedido pedido);
 }
