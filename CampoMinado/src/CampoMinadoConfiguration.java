f6c56d4a75bef301b252ab6664a8626164ceebce
Cenez Araujo <cenezaraujo@gmail.com>
2018-11-24 21:52:02 -0300

    Update

--- CampoMinado/src/CampoMinadoConfiguration.java
+++ CampoMinado/src/CampoMinadoConfiguration.java
@@ -9,9 +9,11 @@ import javax.swing.JFrame;
 
 public class CampoMinadoConfiguration {
 	private CampoMinado campoMinado;
-	public CampoMinadoConfiguration(CampoMinado campoMinado){
+
+	public CampoMinadoConfiguration(CampoMinado campoMinado) {
 		this.campoMinado = campoMinado;
 	}
+
 	public void menuConf() {
 		campoMinado.menuBar.add(campoMinado.menu);
 		campoMinado.menu.add(campoMinado.fechar);
@@ -23,13 +25,14 @@ public class CampoMinadoConfiguration {
 		});
 		campoMinado.add(campoMinado.menuBar, BorderLayout.NORTH);
 	}
+
 	public void insereMinas() {
 		campoMinado.center.setLayout(new GridLayout(campoMinado.lin, campoMinado.col));
 		campoMinado.add(campoMinado.center, BorderLayout.CENTER);
-		for(int i=0;i<campoMinado.lin;i++) {
+		for (int i = 0; i < campoMinado.lin; i++) {
 			campoMinado.minas[i] = new Mina[campoMinado.col];
-			for(int j=0; j<campoMinado.col; j++) {
-				Mina mina = new Mina(i,j);
+			for (int j = 0; j < campoMinado.col; j++) {
+				Mina mina = new Mina(i, j);
 				campoMinado.center.add(mina);
 				campoMinado.minas[i][j] = mina;
 				mina.addActionListener(new ActionListener() {
@@ -39,45 +42,41 @@ public class CampoMinadoConfiguration {
 					}
 				});
 			}
-		} //setar as minas perimetrais
-		for(int i=0;i<campoMinado.lin;i++) {
-			for(int j=0; j<campoMinado.col; j++) {
+		} // setar as minas perimetrais
+		for (int i = 0; i < campoMinado.lin; i++) {
+			for (int j = 0; j < campoMinado.col; j++) {
 				Mina mina = campoMinado.minas[i][j];
 				mina.setPerimetro(campoMinado.minas);
 			}
 		}
 	}
+
 	private void testaMina(Mina mina) {
-		if(mina.isEnabled()) {
-			for(int i=0;i<campoMinado.lin;i++) {
-				for(int j=0; j<campoMinado.col; j++) {
-					if(i==mina.L && j==mina.C) {
-						if(mina.isBomba) {
-							mina.setBackground(Color.BLACK);
-							finaliza();
-						}
-						else
-							mina.setBackground(Color.GRAY);
-						mina.perimetros().forEach(v->{
-							if(v!=null) {
-								if(!v.isBomba)
-									v.setBackground(Color.GRAY);
-								else
-									v.setBackground(Color.WHITE);
-								v.setEnabled(false);
-							}
-						});
-					}
-				}
+		if (mina.isEnabled()) {
+			if (mina.isBomba) {
+				mina.setBackground(Color.BLACK);
+				finaliza();
+				return;
 			}
+			mina.setBackground(Color.GRAY);
+			mina.perimetros().forEach(v -> {
+				if (v != null) {
+					if (!v.isBomba)
+						v.setBackground(Color.GRAY);
+					else
+						v.setBackground(Color.WHITE);
+					v.setEnabled(false);
+				}
+			});
 		}
 		mina.setEnabled(false);
 	}
+
 	private void finaliza() {
-		for(int i=0;i<campoMinado.lin;i++) {
-			for(int j=0; j<campoMinado.col; j++) {
+		for (int i = 0; i < campoMinado.lin; i++) {
+			for (int j = 0; j < campoMinado.col; j++) {
 				Mina mina = campoMinado.minas[i][j];
-				if(mina.isBomba)
+				if (mina.isBomba)
 					mina.setBackground(Color.BLACK);
 				else
 					mina.setBackground(Color.GRAY);
@@ -85,8 +84,9 @@ public class CampoMinadoConfiguration {
 			}
 		}
 	}
+
 	public void dimensionsConfig(final int MAX) {
-		final int PROPORCAO = 30*MAX;
+		final int PROPORCAO = 30 * MAX;
 		campoMinado.setTitle("Jogo do campo minado!!!");
 		campoMinado.setPreferredSize(new Dimension(PROPORCAO, PROPORCAO));
 		campoMinado.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
